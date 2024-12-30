46fbb603565582bf7b0c17eacdbee7c8344b310d
Tarek Mostafa <tarekmostafa.web@gmail.com>
2024-12-30 22:52:51 +0200

    update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProductListing from "./components/ProductListing";
+import Features from "./components/Features";
+import Mission from "./components/Mission";
+import Populars from "./components/Populars";
+import Testimonials from "./components/Testimonials";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
 
+function App() {
   return (
     <>
-      <h1>React App</h1>      
+      <Navbar />
+      <Hero />
+      <ProductListing />
+      <Features />
+      <Mission />
+      <Populars />
+      <Testimonials />
+      <Blogs />
+      <Footer />
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
