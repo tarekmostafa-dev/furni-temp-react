8a752b91ead82eea9da471138fba134f4be7480c
Tarek Mostafa <tarekmostafa.web@gmail.com>
2024-12-31 22:18:20 +0200

    updating footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ProductListing from './components/ProductListing';
 import About from './components/About';
-import Mission from './components/Services';
+import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Blogs from './components/Blogs';
 import Footer from './components/Footer';
@@ -14,7 +14,7 @@ function App() {
       <Hero />
       <ProductListing />
       <About />
-      <Mission />
+      <Services />
       <Testimonials />
       <Blogs />
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,130 +1,216 @@
 import sofa from '../assets/images/sofa.png';
-import { Envelope, Send, Facebook, Twitter, Instagram, Linkedin } from 'react-bootstrap-icons';
+import {
+  Envelope,
+  Send,
+  Facebook,
+  Twitter,
+  Instagram,
+  Linkedin,
+} from 'react-bootstrap-icons';
 
 const Footer = () => {
   return (
-    <footer className="footer-section">
-      <div className="container py-5">
-        {/* Sofa Image and Subscribe Section in Two Columns */}
-        <div className="row align-items-center">
-          {/* Subscribe Section Column */}
-          <div className="col-md-8">
-            <div className="subscription-form">
-              <h3 className="d-flex align-items-center">
-                <span className="me-1">
-                  <Envelope size={20} />
-                </span>
-                <span>Subscribe to Our Newsletter</span>
-              </h3>
-
-              <form action="#" className="row g-3">
-                <div className="col-auto">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Enter your name"
-                  />
-                </div>
-                <div className="col-auto">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter your email"
-                  />
-                </div>
-                <div className="col-auto">
-                  <button className="btn btn-primary">
-                    <Send size={20} />
-                  </button>
-                </div>
-              </form>
-            </div>
+    <footer className='footer-section bg-light'>
+      <div className='container py-5'>
+        {/* Newsletter Section */}
+        <div className='row align-items-center mb-5'>
+          <div className='col-md-9'>
+            <h3 className='d-flex align-items-center mb-3'>
+              <Envelope
+                className='me-2'
+                size={20}
+              />
+              Subscribe to Our Newsletter
+            </h3>
+            <form className='row g-2'>
+              <div className='col-auto flex-grow-1'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Your Name'
+                />
+              </div>
+              <div className='col-auto flex-grow-1'>
+                <input
+                  type='email'
+                  className='form-control'
+                  placeholder='Your Email'
+                />
+              </div>
+              <div className='col-auto'>
+                <button
+                  className='btn btn-primary'
+                  type='submit'
+                >
+                  <Send size={20} />
+                </button>
+              </div>
+            </form>
           </div>
-
-          {/* Sofa Image Column (Smaller and Right-aligned) */}
-          <div className="col-md-4">
-            <img src={sofa} alt="Sofa Image" className="img-fluid" />
-          </div>
-        </div>
-
-        {/* Description Section */}
-        <div className="row g-5 mb-5 mt-5">
-          <div className="col-lg-12">
-            <p className="text-center">
-              Furni is a modern interior design studio dedicated to creating stylish and functional furniture pieces for every space. Our team of designers strives to blend form and function in every piece, ensuring quality, comfort, and timeless appeal for your home.
-            </p>
+          <div className='col-md-3 text-end'>
+            <img
+              src={sofa}
+              alt='Sofa'
+              className='img-fluid'
+            />
           </div>
         </div>
 
-        {/* Social Media Links Section */}
-        <div className="row justify-content-center mb-5">
-          <div className="col-lg-12 text-center">
-            <ul className="list-unstyled custom-social d-flex justify-content-center">
-              <li className="mx-3">
-                <a href="#" className="text-dark">
-                  <Facebook size={30} />
+        {/* Footer Links Section */}
+        <div className='row mb-4'>
+          {/* First Column */}
+          <div className='col-md-4'>
+            <ul className='list-unstyled'>
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  About Us
                 </a>
               </li>
-              <li className="mx-3">
-                <a href="#" className="text-dark">
-                  <Twitter size={30} />
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Services
                 </a>
               </li>
-              <li className="mx-3">
-                <a href="#" className="text-dark">
-                  <Instagram size={30} />
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Blog
                 </a>
               </li>
-              <li className="mx-3">
-                <a href="#" className="text-dark">
-                  <Linkedin size={30} />
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Second Column */}
+          <div className='col-md-4'>
+            <ul className='list-unstyled'>
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Our Team
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Leadership
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='text-dark'
+                >
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Third Column: Vertical Social Links */}
+          <div className='col-md-4'>
+            <ul className='list-unstyled'>
+              <li className='mb-2'>
+                <a
+                  href='#'
+                  className='text-dark d-flex align-items-center'
+                >
+                  <Facebook
+                    className='me-2'
+                    size={25}
+                  />{' '}
+                  Facebook
+                </a>
+              </li>
+              <li className='mb-2'>
+                <a
+                  href='#'
+                  className='text-dark d-flex align-items-center'
+                >
+                  <Twitter
+                    className='me-2'
+                    size={25}
+                  />{' '}
+                  Twitter
+                </a>
+              </li>
+              <li className='mb-2'>
+                <a
+                  href='#'
+                  className='text-dark d-flex align-items-center'
+                >
+                  <Instagram
+                    className='me-2'
+                    size={25}
+                  />{' '}
+                  Instagram
+                </a>
+              </li>
+              <li className='mb-2'>
+                <a
+                  href='#'
+                  className='text-dark d-flex align-items-center'
+                >
+                  <Linkedin
+                    className='me-2'
+                    size={25}
+                  />{' '}
+                  LinkedIn
                 </a>
               </li>
             </ul>
           </div>
         </div>
 
-        {/* Links Section */}
-        <div className="row g-5 mb-5">
-          <div className="col-md-3">
-            <ul className="list-unstyled">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <ul className="list-unstyled">
-              <li><a href="#">Support</a></li>
-              <li><a href="#">Knowledge Base</a></li>
-              <li><a href="#">Live Chat</a></li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <ul className="list-unstyled">
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Our Team</a></li>
-              <li><a href="#">Leadership</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <ul className="list-unstyled">
-              <li><a href="#">Nordic Chair</a></li>
-              <li><a href="#">Kruzo Aero</a></li>
-              <li><a href="#">Ergonomic Chair</a></li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Copyright Section - Full Width */}
-        <div className="copyright-section bg-dark text-white py-4 w-100">
-          <div className="container text-center">
-            <p className="mb-0">
-              &copy; 2024 Furni. All Rights Reserved. Designed with love by
-              <a href="https://untree.co" target="_blank" rel="noopener noreferrer" className="text-white"> Untree.co</a> 
-              Distributed By <a href="https://themewagon.com" target="_blank" rel="noopener noreferrer" className="text-white">ThemeWagon</a>
+        {/* Copyright Section */}
+        <div className='bg-dark text-white py-3'>
+          <div className='text-center'>
+            <p className='mb-0'>
+              &copy; 2024 Furni. All Rights Reserved. Designed by
+              <a
+                href='https://untree.co'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-white ms-1'
+              >
+                Untree.co
+              </a>
+              . Distributed by
+              <a
+                href='https://themewagon.com'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-white ms-1'
+              >
+                ThemeWagon
+              </a>
             </p>
           </div>
         </div>
